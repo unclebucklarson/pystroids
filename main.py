6e5fcec5d08099b55f3d6000e9a1cd46067bf68e
Scott Larson <unclebuck.larson@yahoo.com>
2024-09-19 10:52:25 -0600

    Got closing window working

--- main.py
+++ main.py
@@ -7,10 +7,18 @@ from constants import *
 
 def main():
     pygame.init()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     while True:
         screen.fill('black')
         pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+
+
 
 if __name__ == "__main__":
     main()
